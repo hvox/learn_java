0ceba7c3ab2643409fe260c267a4324095a4a490
Uladzislau Khamkou <kham@tuta.io>
2022-03-15 20:12:06 +0300

    implement removing prefix from string

--- StringsDemo.java
+++ StringsDemo.java
@@ -4,6 +4,8 @@ public class StringsDemo {
         System.out.println(str1 + " is palindrome: " + isPalindrome(str1));
         String str2 = "heleh";
         System.out.println(str2 + " is palindrome: " + isPalindrome(str2));
+        String str3 = "hello, world!!";
+        System.out.printf("%s - %s = %s\n", str3, str1, removePrefix(str3, str1));
     }
 
     static boolean isPalindrome(String str) {
@@ -12,5 +14,21 @@ public class StringsDemo {
             if (str.charAt(i) != str.charAt(n - i - 1))
                 return false;
         return true;
+    }
+
+    static boolean isPrefix(String str, String prefix) {
+        if (prefix.length() > str.length())
+            return false;
+        int n = prefix.length();
+        for (int i = 0; i < n; i++)
+            if (str.charAt(i) != prefix.charAt(i))
+                return false;
+        return true;
+    }
+
+    static String removePrefix(String str, String prefix) {
+        if (!isPrefix(str, prefix))
+            return str;
+        return str.substring(prefix.length());
     }
 }
