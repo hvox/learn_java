c48f57d36cc160ce94cba28accd7258222e77614
Uladzislau Khamkou <kham@tuta.io>
2022-05-14 23:58:41 +0300

    trick the typechecker

---
+++ Wat.java
@@ -0,0 +1,6 @@
+public class Wat {
+    public static void main(String[] args) {
+        Object[] array = new Integer[1];
+        array[0] = "Hello there! I am going to kill everything!";
+    }
+}
